179fa554781a6b589f9f173f96dfc35d2cd1be36
Corentin Latappy <corentin.lat@gmail.com>
2024-11-06 18:38:00 +0100

    🚧 Starting to add FullPage library

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,29 @@
 <template>
-    <h1>Corentin Latappy</h1>
+    <ProfileInfo />
+    <NavigationMenu />
 
     <full-page :options="options">
-        <div class="section">First</div>
-        <div class="section">Second</div>
-        <div class="section">Third</div>
+        <MyBio class="section" />
+        <MyPublications class="section" />
+        <MyTeachings class="section" />
+        <MyPersonalProjects class="section" />
     </full-page>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue';
 
-const options = ref({
+import MyBio from '@/components/MyBio.vue';
+import MyPersonalProjects from '@/components/MyPersonalProjects.vue';
+import MyPublications from '@/components/MyPublications.vue';
+import MyTeachings from '@/components/MyTeachings.vue';
+import NavigationMenu from '@/components/NavigationMenu.vue';
+import ProfileInfo from '@/components/ProfileInfo.vue';
 
+const options = ref({
+    anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage'],
+    menu: '#myMenu',
 });
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
---
+++ src/components/MyBio.vue
@@ -0,0 +1,7 @@
+<template>
+    <h1>Bio</h1>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
