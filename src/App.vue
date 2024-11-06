a55278754c35fd67457b37b886b002181806e50e
Latappy Corentin <corentin.latappy@etu.u-bordeaux.fr>
2024-11-06 16:35:18 +0100

    🎉 Initializing project structure

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1>Corentin Latappy</h1>
+
+    <full-page :options="options">
+        <div class="section">First</div>
+        <div class="section">Second</div>
+        <div class="section">Third</div>
+    </full-page>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const options = ref({
+
+});
+</script>
+
+<style scoped>
+</style>
