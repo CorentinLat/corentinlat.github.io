e131fb171fdba1b131549148471e4a89a7e90389
Latappy Corentin <corentin.latappy@etu.u-bordeaux.fr>
2024-11-06 23:20:26 +0100

    🚧 Main in construction

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-
-import 'vue-fullpage.js/dist/style.css';
-import VueFullPage from 'vue-fullpage.js';
-
-const app = createApp(App);
-app.use(VueFullPage);
-app.mount('#app');
