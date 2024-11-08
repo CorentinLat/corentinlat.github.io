2751e9bb85ce9ee13abd2909c354ff3a7c508236
Corentin Latappy <corentin.lat@gmail.com>
2024-11-08 14:55:59 +0100

    📱 Adding responsive to FullScreenSection.vue

--- src/components/layout/FullScreenSection.vue
+++ src/components/layout/FullScreenSection.vue
@@ -15,6 +15,13 @@ defineProps<{ title?: string }>();
 </script>
 
 <style lang="scss" scoped>
+@mixin scrollableElementSize($scrollableElementWidth, $scrollableElementPadding, $borderWidth) {
+    width: calc($scrollableElementWidth - $scrollableElementPadding * 2 - $borderWidth * 2);
+    margin: 0 calc((100% - $scrollableElementWidth) / 2);
+    padding: $scrollableElementPadding;
+    border: $borderWidth solid black;
+}
+
 .full-screen-section-component {
     display: flex;
     flex-direction: column;
@@ -27,19 +34,29 @@ defineProps<{ title?: string }>();
     }
 
     .scrollable-element {
-        $scrollableElementPadding: 20px;
-
-        width: calc(80% - $scrollableElementPadding * 2);
-        margin: 0 10%;
-        padding: $scrollableElementPadding;
+        @include scrollableElementSize(80%, 20px, 1px);
         overflow-y: scroll;
 
         display: flex;
         flex-direction: column;
         gap: 25px;
 
-        border: 1px solid black;
         border-radius: 15px;
+    }
+}
+
+@media (max-width: 1024px) {
+    .full-screen-section-component {
+        margin: 10px;
+
+        .title {
+            width: 100%;
+            margin-bottom: 10px;
+        }
+
+        .scrollable-element {
+            @include scrollableElementSize(100%, 10px, 1px);
+        }
     }
 }
 </style>
